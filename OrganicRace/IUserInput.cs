8076b849e033c3154eff3084ba774971187506f9
Kat <pararapist@mail.lv>
2016-06-12 17:25:55 +0300

    Some sort of testing gimmick.
    Before I fuck up forever.

---
+++ OrganicRace/IUserInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganicRace
+{
+	public interface IUserInput
+	{
+		void GetUserInput(string action, string @object, string subject);
+	}
+}
---
+++ OrganicRace/UserInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganicRace
+{
+	public class UserInput : IUserInput
+	{
+		public void GetUserInput (string action, string @object, string subject)
+		{
+
+		}
+	}
+}
